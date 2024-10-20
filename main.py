268849b944f411cc59d27f7e2aa76d64e4a9410d
Buravit Yenjit <khiaokrub@gmail.com>
2024-10-20 17:07:18 +0700

    4. Define a Factory for Movies (MovieCatalog)

--- main.py
+++ main.py
@@ -4,24 +4,39 @@
 from movie import Movie
 from rental import Rental
 from customer import Customer
+from movie import Movie, MovieCatalog
 
 def make_movies():
     """Some sample movies."""
+    catalog = MovieCatalog()
     movies = [
-        Movie("Air", Movie.NEW_RELEASE),
-        Movie("Oppenheimer", Movie.REGULAR),
-        Movie("Frozen", Movie.CHILDRENS),
-        Movie("Bitconned", Movie.NEW_RELEASE),
-        Movie("Particle Fever", Movie.REGULAR)
+        # Movie("Air", Movie.NEW_RELEASE),
+        # Movie("Oppenheimer", Movie.REGULAR),
+        # Movie("Frozen", Movie.CHILDRENS),
+        # Movie("Bitconned", Movie.NEW_RELEASE),
+        # Movie("Particle Fever", Movie.REGULAR)
+        catalog.get_movie("Air"),
+        catalog.get_movie("Oppenheimer"),
+        catalog.get_movie("Frozen"),
+        catalog.get_movie("Bitconned"),
+        catalog.get_movie("Particle Fever")
     ]
     return movies
 
 
 if __name__ == '__main__':
-    # Create a customer with some rentals
-    customer = Customer("Edward Snowden")
-    days = 1
-    for movie in make_movies():
-        customer.add_rental(Rental(movie, days, movie.price_strategy))
-        days = (days + 2) % 5 + 1
-    print(customer.statement())
+    # # Create a customer with some rentals
+    # customer = Customer("Edward Snowden")
+    # days = 1
+    # for movie in make_movies():
+    #     customer.add_rental(Rental(movie, days, movie.price_strategy))
+    #     days = (days + 2) % 5 + 1
+    # print(customer.statement())
+    # Get the Singleton Movie Catalog
+    catalog = MovieCatalog()
+    # Get the first movie named 'Mulan'
+    movie = catalog.get_movie("Mulan")
+    # Get 'Mulan' released in 1998
+    old_movie = catalog.get_movie("Mulan", 1998)
+    print(old_movie)
+    print(movie)
--- movie.py
+++ movie.py
@@ -1,6 +1,42 @@
+import csv
+from calendar import month
 from typing import Collection
-
 from dataclasses import dataclass
+
+class MovieCatalog:
+    _instances = None
+
+    def __new__(cls):
+        """Create a new MovieCatalog if not already exist."""
+        if cls._instances is None:
+            cls._instances = super(MovieCatalog, cls).__new__(cls)
+            cls._instances._movie_list = cls.load_movie_data()
+        return cls._instances
+
+    @staticmethod
+    def load_movie_data():
+        """Load movie data from a CSV file."""
+        movies = []
+        with open("movies.csv", encoding="utf-8") as movie_file:
+            movie_reader = csv.DictReader(movie_file)
+            next(movie_reader)
+            for _, movie_data in enumerate(movie_reader):
+                try:
+                    movie = Movie(
+                        movie_data['title'],
+                        int(movie_data['year']),
+                        movie_data['genres'].split('|')
+                    )
+                    movies.append(movie)
+                except (TypeError, ValueError):
+                    continue
+
+        return movies
+
+    def get_movie(self, title, year=None):
+        for movie in self._movie_list:
+            if title.lower() == movie.title.lower() and (not year or year == movie.year):
+                return movie
 
 @dataclass(frozen=True)
 class Movie:
